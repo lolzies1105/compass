86c3103d221ca3313cfa3850009f6cf2f38d3407
lolzies1105 <140378613+lolzies1105@users.noreply.github.com>
2023-07-24 15:05:39 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let bearing = 0
+basic.forever(function () {
+    bearing = input.compassHeading()
+    if (bearing < 45 || bearing > 315) {
+        basic.showString("N")
+    } else {
+        basic.showString(" ")
+    }
+})
